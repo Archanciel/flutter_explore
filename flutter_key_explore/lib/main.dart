7af99205e5b475634ebbcad734d4221991b2f25a
Archanciel <jp.schnyder@gmail.com>
2022-05-31 06:29:30 +0200

    init commit

---
+++ flutter_key_explore/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_key_explore/page/advanced_key_page.dart';
+import 'package:flutter_key_explore/page/basic_key_page.dart';
+import 'package:flutter_key_explore/utils.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Value Key';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: MainPage(),
+      );
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        bottomNavigationBar: buildBottomBar(),
+        body: buildPages(),
+      );
+
+  Widget buildBottomBar() {
+    final style = TextStyle(color: Colors.white);
+
+    return BottomNavigationBar(
+      backgroundColor: Theme.of(context).primaryColor,
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.white70,
+      currentIndex: index,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.beach_access),
+          label: 'Basics',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.expand_less),
+          label: 'Advanced',
+        ),
+      ],
+      onTap: (int index) => setState(() => this.index = index),
+    );
+  }
+
+  Widget buildPages() {
+    switch (index) {
+      case 0:
+        return BasicKeyPage();
+      case 1:
+        return AdvancedKeyPage();
+      default:
+        return Container();
+    }
+  }
+}
