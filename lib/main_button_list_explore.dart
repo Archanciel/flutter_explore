0388eae2db4dca25f38784d27c28f0aebde8877b
Archanciel <jp.schnyder@gmail.com>
2022-06-08 08:45:17 +0200

    Create main_button_list_explore.dart

---
+++ lib/main_button_list_explore.dart
@@ -0,0 +1,62 @@
+// https://stackoverflow.com/questions/72043195/how-to-define-a-not-rounded-flutter-textbutton
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatefulWidget {
+  @override
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextButton(
+                child: const Text(''),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+                onPressed: () {
+                  _showPopupMenu();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _showPopupMenu() {
+    showMenu<String>(
+      context: context,
+      position: RelativeRect.fromLTRB(25.0, 25.0, 0.0,
+          0.0), //position where you want to show the menu on screen
+      items: [
+        PopupMenuItem<String>(child: const Text('menu option 1'), value: '1'),
+        PopupMenuItem<String>(child: const Text('menu option 2'), value: '2'),
+        PopupMenuItem<String>(child: const Text('menu option 3'), value: '3'),
+      ],
+      elevation: 8.0,
+    ).then<void>((String? itemSelected) {
+      if (itemSelected == null) return;
+
+      if (itemSelected == "1") {
+        //code here
+      } else if (itemSelected == "2") {
+        //code here
+      } else {
+        //code here
+      }
+    });
+  }
+}
