0263fb8b73161567d26643eac82ec2f0d1ab43b1
Archanciel <jp.schnyder@gmail.com>
2022-08-05 17:39:21 +0200

    now works. BINGO !

--- lib/flutter_stateful_widget_passing_data_explore.dart
+++ lib/flutter_stateful_widget_passing_data_explore.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -28,12 +29,43 @@ class MyStatefullApp extends StatefulWidget {
 
 class _MyStatefullAppState extends State<MyStatefullApp> {
   final Map<String, dynamic> widgetOneValueMap = {
+    'name': 'one',
     'text': 'widget one init text',
     'value': 1,
   };
 
+  final Map<String, dynamic> widgetTwoValueMap = {
+    'name': 'two',
+    'text': 'widget two init text',
+    'value': 2,
+  };
+
+  late StatefullWidget _statefullWidgetOne;
+  late StatefullWidget _statefullWidgetTwo;
+
+  @override
+  void initState() {
+    _statefullWidgetOne = StatefullWidget(
+      widgetValueMap: widgetOneValueMap,
+      onSubmitFunction: _widgetOnSubmitFunction,
+    );
+    _statefullWidgetTwo = StatefullWidget(
+      widgetValueMap: widgetTwoValueMap,
+      onSubmitFunction: _widgetOnSubmitFunction,
+    );
+    super.initState();
+  }
+
   void _widgetOnSubmitFunction(Map<String, dynamic> widgetReturnedValueMap) {
     print(widgetReturnedValueMap);
+
+    if (widgetReturnedValueMap['name'] == 'one') {
+      _statefullWidgetTwo.updateWidgetValues(
+          widgetModifiedValueMap: widgetReturnedValueMap);
+    } else {
+      _statefullWidgetOne.updateWidgetValues(
+          widgetModifiedValueMap: widgetReturnedValueMap);
+    }
   }
 
   @override
@@ -48,52 +80,68 @@ class _MyStatefullAppState extends State<MyStatefullApp> {
           padding: EdgeInsets.all(15.0),
           child: Column(
             children: [
-              StatefullWidgetOne(
-                widgetValueMap: widgetOneValueMap,
-                onSubmitFunction: _widgetOnSubmitFunction,
+              _statefullWidgetOne,
+              const SizedBox(
+                height: 30.0,
               ),
+              _statefullWidgetTwo,
             ],
           )),
     );
   }
 }
 
-class StatefullWidgetOne extends StatefulWidget {
+class StatefullWidget extends StatefulWidget {
   // widget values returned to the widget including instance in order
   // for it to compute on the modified values and/or transmit them
   // to the other wodgets
   Map<String, dynamic> widgetValueMap = {};
 
   void Function(Map<String, dynamic> widgetReturnedValueMap) onSubmitFunction;
-  void updateWidgetValues(
-      {required Map<String, dynamic> widgetModifiedValueMap}) {
-    widgetValueMap.updateAll((key, value) => widgetModifiedValueMap[key]);
-  }
 
   /// onSubmitFunction  passed function which is called each time the
   /// user modified a widget editable value.
   /// inputValueChangedFunction  passed function which is called each time the
   /// by the widget includer each time the widget has to get modified
   /// input values.
-  StatefullWidgetOne({
+  StatefullWidget({
     Key? key,
     required Map<String, dynamic> this.widgetValueMap,
     required void Function(Map<String, dynamic> widgetReturnedValueMap)
         this.onSubmitFunction,
   }) : super(key: key);
 
+  late _StatefullWidgetState state;
+
   @override
-  State<StatefullWidgetOne> createState() => _StatefullWidgetOneState();
+  _StatefullWidgetState createState() {
+    state = _StatefullWidgetState();
+
+    return state;
+  }
+
+  void updateWidgetValues(
+      {required Map<String, dynamic> widgetModifiedValueMap}) {
+    for (MapEntry<String, dynamic> entry in widgetModifiedValueMap.entries) {
+      if (entry.key != 'name') {
+        widgetValueMap[entry.key] = entry.value;
+      }
+    }
+
+    state.updateWidgetValues();
+  }
 }
 
-class _StatefullWidgetOneState extends State<StatefullWidgetOne> {
+class _StatefullWidgetState extends State<StatefullWidget> {
   late final TextEditingController _controllerText;
   late final TextEditingController _controllerValue;
 
   @override
   void initState() {
-    _controllerText = TextEditingController(text: widget.widgetValueMap['text']);
-    _controllerValue = TextEditingController(text: widget.widgetValueMap['value'].toString());
+    _controllerText =
+        TextEditingController(text: widget.widgetValueMap['text']);
+    _controllerValue =
+        TextEditingController(text: widget.widgetValueMap['value'].toString());
 
     super.initState();
   }
@@ -104,6 +152,12 @@ class _StatefullWidgetOneState extends State<StatefullWidgetOne> {
     _controllerValue.dispose();
 
     super.dispose();
+  }
+
+  void updateWidgetValues() {
+    _controllerText.text = widget.widgetValueMap['text'];
+    _controllerValue.text = widget.widgetValueMap['value'].toString();
+    setState(() {});
   }
 
   @override
@@ -138,7 +192,7 @@ class _StatefullWidgetOneState extends State<StatefullWidgetOne> {
               width: 15,
             ),
             SizedBox(
-              width: 30.0,
+              width: 40.0,
               child: TextField(
                 style: const TextStyle(
                   fontSize: 20.0,
