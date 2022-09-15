38a341a220a5070f6ef30411ced4df4041c1cde8
Archanciel <jp.schnyder@gmail.com>
2022-09-15 22:54:47 +0200

    Update flutter_date_time_pickert_test.dart

--- flutter_widget_unit_test/test/flutter_date_time_pickert_test.dart
+++ flutter_widget_unit_test/test/flutter_date_time_pickert_test.dart
@@ -1,6 +1,7 @@
 // Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+// https://github.com/flutter/flutter/blob/master/packages/flutter/test/material/date_picker_test.dart
 
 import 'dart:ui';
 
