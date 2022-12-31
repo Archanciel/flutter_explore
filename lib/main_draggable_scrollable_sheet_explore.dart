bf76ac650a4ab496baad0f2bb7e8e630d96ecf0f
Archanciel <jp.schnyder@gmail.com>
2022-12-31 17:21:01 +0100

    yes WORKS !

---
+++ lib/main_draggable_scrollable_sheet_explore.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Draggable scrollable sheet explore',
+      theme: ThemeData.dark(),
+      home: const MyStatefulApp(),
+    );
+  }
+}
+
+class MyStatefulApp extends StatefulWidget {
+  const MyStatefulApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulApp> createState() => _MyStatefulAppState();
+}
+
+class _MyStatefulAppState extends State<MyStatefulApp> {
+  String _status = 'Wake up';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+        'Draggable scrollable sheet explore',
+      )),
+      body: DraggableScrollableSheet(
+        initialChildSize: 1.0,
+        minChildSize: 0.5,
+        builder: (BuildContext context, ScrollController scrollController) {
+          return SingleChildScrollView(
+            child: GestureDetector(
+              // enables that when clicking above or below a
+              // TextField, the keyboard is hidden. Otherwise,
+              // the keyboard remains open !
+              onTap: () {
+                // call this method here to hide soft keyboard
+                FocusManager.instance.primaryFocus?.unfocus();
+              },
+              child: Container(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text('One'),
+                    TextField(
+                      keyboardType: TextInputType.multiline,
+                      minLines: 4,
+                      maxLines: 4,
+                    ),
+                    Text('Two'),
+                    TextField(
+                      keyboardType: TextInputType.multiline,
+                      minLines: 4,
+                      maxLines: 4,
+                    ),
+                    Text('Three'),
+                    TextField(
+                      keyboardType: TextInputType.multiline,
+                      minLines: 4,
+                      maxLines: 4,
+                    ),
+                    Text('Four'),
+                    TextField(
+                      keyboardType: TextInputType.multiline,
+                      minLines: 4,
+                      maxLines: 4,
+                    ),
+                    Text('Five'),
+                    TextField(
+                      keyboardType: TextInputType.multiline,
+                      minLines: 4,
+                      maxLines: 4,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
