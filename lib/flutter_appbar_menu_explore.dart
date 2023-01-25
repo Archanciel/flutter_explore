c495fc2d017d0f04ee4a21fee0405dc9a4579b0d
Archanciel <jp.schnyder@gmail.com>
2023-01-25 18:33:25 +0100

    Update flutter_appbar_menu_explore.dart

--- lib/flutter_appbar_menu_explore.dart
+++ lib/flutter_appbar_menu_explore.dart
@@ -21,34 +21,92 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Popup Menu on AppBar"),
+          title: const Text("Popup Menu on AppBar"),
           backgroundColor: Colors.blue,
+          elevation: 0,
           actions: [
             PopupMenuButton(
                 // add icon, by default "3 dot" icon
                 // icon: Icon(Icons.book)
                 itemBuilder: (context) {
               return [
-                PopupMenuItem<int>(
+                const PopupMenuItem<int>(
                   value: 0,
                   child: Text("My Account"),
                 ),
-                PopupMenuItem<int>(
+                const PopupMenuItem<int>(
                   value: 1,
                   child: Text("Settings"),
                 ),
-                PopupMenuItem<int>(
+                const PopupMenuItem<int>(
                   value: 2,
                   child: Text("Logout"),
                 ),
+                const PopupMenuItem<int>(
+                  value: 3,
+                  child: Text("Three"),
+                ),
+                const PopupMenuItem<int>(
+                  value: 4,
+                  child: Text("Four"),
+                ),
+                const PopupMenuItem<int>(
+                  value: 5,
+                  child: Text("Five"),
+                ),
+                const PopupMenuItem<int>(
+                  value: 6,
+                  child: Text("Six"),
+                ),
+                const PopupMenuItem<int>(
+                  value: 7,
+                  child: Text("Seven"),
+                ),
               ];
             }, onSelected: (value) {
-              if (value == 0) {
-                print("My account menu is selected.");
-              } else if (value == 1) {
-                print("Settings menu is selected.");
-              } else if (value == 2) {
-                print("Logout menu is selected.");
+              switch (value) {
+                case 0:
+                  {
+                    print("My account menu is selected.");
+                    break;
+                  }
+                case 1:
+                  {
+                    print("Settings menu is selected.");
+                    break;
+                  }
+                case 2:
+                  {
+                    print("Logout menu is selected.");
+                    break;
+                  }
+                case 3:
+                  {
+                    print("3 is selected.");
+                    break;
+                  }
+                case 4:
+                  {
+                    print("4 is selected.");
+                    break;
+                  }
+                case 5:
+                  {
+                    print("5 is selected.");
+                    break;
+                  }
+                case 6:
+                  {
+                    print("6 is selected.");
+                    break;
+                  }
+                case 7:
+                  {
+                    print("7 is selected.");
+                    break;
+                  }
+                default:
+                  {}
               }
             }),
           ],
